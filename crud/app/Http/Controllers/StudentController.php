7f128f3707ab9334a45c6c134cc13c5d158bb250
maneeshay <mansa.yadav05@gmail.com>
2022-07-25 12:22:40 +0545

    Crud Complete

---
+++ crud/app/Http/Controllers/StudentController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Student::all();
+        return view('home',['students'=>$students]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request )
+    {
+        $student = new Student;
+        $student->name = $request->name;
+        $student->address = $request->address;
+        $student->marks = $request->marks;
+        $student->save();
+        return redirect(route('index'))->with('status','Student added successfully!!') ;
+    }
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $student = Student::find($id);
+        return view('editform',['student'=>$student]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $student = Student::find($id);
+        $student->name = $request->name;
+        $student->address = $request->address;
+        $student->marks = $request->marks;
+        $student->save();
+        return redirect(route('index'))->with('status','Student updated successfully!!') ;;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Student::destroy($id);
+        return redirect(route('index'))->with('status','Student deleted successfully!!') ;;
+    }
+}
